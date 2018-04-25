6854f167beb3081afaa250566e41c361752d5eb9
wijittrapo <wijittrapo@srru.ac.th>
2018-04-25 14:45:35 +0700

    Initial commit

---
+++ app/src/main/java/org/surincampus/org/buribum/MainActivity.java
@@ -0,0 +1,26 @@
+package org.surincampus.org.buribum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.surincampus.org.buribum.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment,new MainFragment()).commit();
+
+        }
+    }   //Main method
+
+    //Add Fragment
+
+
+}   //Main Class
+
+
---
+++ app/src/main/java/org/surincampus/org/buribum/fragment/MainFragment.java
@@ -0,0 +1,25 @@
+package org.surincampus.org.buribum.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.ViewUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.surincampus.org.buribum.R;
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+
+        return view;
+
+    }
+}  //Main Class
